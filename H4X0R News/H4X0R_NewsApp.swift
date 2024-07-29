de1e84c7d4b7ca549811fb072f29dbf26d7f1db9
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-07-29 22:57:46 +0900

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Anastasia Grey on 2024/07/29.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
