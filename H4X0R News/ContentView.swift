c2d01def50a88bd2514808c945b20625cd7501ca
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-07-30 09:27:06 +0900

    List の追加

--- H4X0R News/ContentView.swift
+++ H4X0R News/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .navigationTitle("H4X0R")
+            
         }
-        .padding()
     }
 }
 
