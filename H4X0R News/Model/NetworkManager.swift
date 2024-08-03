ef3cf07306cc93895075c117c8b795c0df646b65
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-08-03 22:22:30 +0900

    DIspatchQueue の追加

--- H4X0R News/Model/NetworkManager.swift
+++ H4X0R News/Model/NetworkManager.swift
@@ -8,7 +8,9 @@
 import Foundation
 
 
-class NetworkManager {
+class NetworkManager: ObservableObject {
+    
+    @Published var posts = [Post]()
     
     func fetchData() {
         if let url = URL(string: "https://hn.algolia.com/api/v1/search?tags=front_page") {
@@ -16,9 +18,19 @@ class NetworkManager {
             let task = session.dataTask(with: url) { data, response, error in
                 if error == nil {
                     let decoder = JSONDecoder()
-                    
+                    if let safeData = data {
+                        do {
+                            let results = try decoder.decode(Results.self, from: safeData)
+                            DispatchQueue.main.async{
+                                self.posts = results.hits
+                            }
+                        } catch {
+                            print(error)
+                        }
+                    }
                 }
             }
+            task.resume()
         }
     }
 }
