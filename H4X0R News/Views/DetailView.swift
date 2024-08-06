2956f61142f47dfd47fc03e27b05751cf0697ed6
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-08-06 22:03:32 +0900

    NavigationLink の追加

---
+++ H4X0R News/Views/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  H4X0R News
+//
+//  Created by Anastasia Grey on 2024/08/05.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView(url: "")
+}
