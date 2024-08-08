3c5efb81668a36897fcbb192d7d1f12cc400084e
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-08-08 21:15:20 +0900

    refactoring

--- H4X0R News/Views/DetailView.swift
+++ H4X0R News/Views/DetailView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import WebKit
 
 struct DetailView: View {
     
@@ -19,22 +18,4 @@ struct DetailView: View {
 
 #Preview {
     DetailView(url: "")
-}
-
-struct WebView: UIViewRepresentable {
-    
-    let urlString: String?
-    
-    func makeUIView(context: Context) -> WKWebView {
-        return WKWebView()
-    }
-    
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        if let safeString = urlString {
-            if let url = URL(string: safeString) {
-                let request = URLRequest(url: url)
-                uiView.load(request)
-            }
-        }
-    }
 }
